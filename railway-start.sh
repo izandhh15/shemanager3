a22ce4403b736db996a7af616f4af2151ddaba4c
izandhh15 <izandelghin@gmail.com>
2026-03-17 01:48:09 +0100

    Update railway-start.sh to fix line 42 for startup script.

--- railway-start.sh
+++ railway-start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Other lines of the script...
+# Startup script for the application
+# Replace line 42 with the corrected command
 
-# Updated line
+# other initial commands
+
+# line 42
 exec php artisan serve --host=0.0.0.0 --port=${PORT:-8080}
+
+# other commands to start the application
